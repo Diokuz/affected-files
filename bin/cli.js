6a725f1e5ce017d9747600f394faa2c60ab8c7d0
Dmitriy Kuznetsov <d.o.kuznetsov@tinkoff.ru>
2019-07-31 16:34:48 +0300

    Fix filter, better cli log

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 
 const getAffectedFiles = require('..').default
 
-const pattern = process.argv[2] || ''
+const pattern = process.argv[2] || '**/*'
 
 console.log('pattern: ', pattern)
 
@@ -10,10 +10,17 @@ if (pattern.length > 0 && pattern.indexOf('*') === -1) {
   console.log('No asterisk found. Forgot to add quotes around pattern?')
 }
 
-const files = getAffectedFiles(pattern)
-
-if (!files.length) {
-  console.log(`Nothing found!`)
-} else {
-  console.log('Affected files:\n\n', files.join('\n'), `total: ${files.length}`)
+async function run() {
+  const files = await getAffectedFiles(pattern)
+  
+  if (!files.length) {
+    console.log(`Nothing found!`)
+  } else {
+    console.log('Affected files:\n\n', files.join('\n'), `\n\ntotal: ${files.length}`)
+  }
 }
+
+run().catch(e => {
+  process.exitCode = 1
+  console.error(`failed to get affected files`)
+})
